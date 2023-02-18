cfca7bacef15209edea3e787cdacfbb5369f63da
Tanya <tan_semenuk@mail.ru>
2023-02-18 22:44:20 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 10000; //цена билета
+
+        int miles = 20; //цена 1 мили
+
+        int bonus = price / miles;
+
+        System.out.println((price / miles) + "bonus");
+    }
+}
